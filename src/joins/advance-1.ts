86b0b7c0fd2677a68a8dd1cf9b84886ae4a60b06
kamal9580 <nileshprince3118@gmail.com>
2024-08-01 23:55:35 +0530

    learn join

--- src/joins/advance-1.ts
+++ src/joins/advance-1.ts
@@ -1,21 +1,24 @@
-// import { getClient } from "../utils";
+import { getClient } from "../utils";
 
-// // Get all todos for a give user
-// // This needs to ensure that every user comes atleast once
-// async function getUserAndTodosWithJoin(userId: number) {
-//     const client = await getClient();
+// Get all todos for a give user
+// This needs to ensure that every user comes atleast once
+async function getUserAndTodosWithJoin(userId: number) {
+    const client = await getClient();
 
-//     const joinQuery = `
-//         SELECT users.*, todos.title, todos.description, todos.done
-//         FROM users
-//         LEFT JOIN todos ON users.id = todos.user_id
-//         WHERE users.id = $1;
-//     `;
+    const joinQuery = `
+        SELECT users.*, todos.title, todos.description, todos.done
+        FROM users
+        LEFT JOIN todos ON users.id = todos.user_id
+        WHERE users.id = $1;
+    `;
+    //iska mtlab hai ki users se sara aur todos se title,description,done jha ki 
+    //users.id = todos.user_id aur jiska user id 1 hai .
+    //mtlab user id one ka ka todos me find jiska user id one hai 
 
-//     const res = await client.query(joinQuery, [userId]);
-//     const results = res.rows;
+    const res = await client.query(joinQuery, [userId]);
+    const results = res.rows;
 
-//     console.log("User and Todos:", results);
-// }
+    console.log("User and Todos:", results);
+}
 
-// getUserAndTodosWithJoin(1)
+getUserAndTodosWithJoin(1)
--- src/joins/advance-3.ts
+++ src/joins/advance-3.ts
@@ -1,18 +1,18 @@
-// import { getClient } from "../utils";
+import { getClient } from "../utils";
 
-// async function getAllTodosWithUserDetails() {
-//     const client = await getClient();
+async function getAllTodosWithUserDetails() {
+    const client = await getClient();
 
-//     const joinQuery = `
-//         SELECT todos.*, users.email, users.password
-//         FROM todos
-//         JOIN users ON todos.user_id = users.id;
-//     `;
+    const joinQuery = `
+        SELECT todos.*, users.email, users.password
+        FROM todos
+        JOIN users ON todos.user_id = users.id;
+    `;
 
-//     const res = await client.query(joinQuery);
-//     const results = res.rows;
+    const res = await client.query(joinQuery);
+    const results = res.rows;
 
-//     console.log("Todos with User Details:", results);
-// }
+    console.log("Todos with User Details:", results);
+}
 
-// getAllTodosWithUserDetails();
+getAllTodosWithUserDetails();
